b1ec2cdf8fcc0020fbf4590eb146c8e2f01f30e6
Edoardo Olivieri <diddo.42@hotmail.it>
2019-03-27 15:37:10 +0000

    database done

--- db/seeds.rb
+++ db/seeds.rb
@@ -23,7 +23,21 @@ puts 'Finished ingredients!'
 puts 'Creating cocktails...'
 
 cocktail = Cocktail.new( name: "Mojito" )
-cocktail.remote_photo_url = "https://www.giallozafferano.it/images/ricette/5/570/foto_hd/hd450x300.jpg"
+cocktail.remote_photo_url = "https://cdn.liquor.com/wp-content/uploads/2018/09/04153106/mojito-720x720-recipe.jpg"
 cocktail.save
+
+cocktail = Cocktail.new( name: "
+Espresso martini" )
+cocktail.remote_photo_url = "https://cdn.diffords.com/contrib/stock-images/2016/8/34/2016514176701b5d0ff8e94868a69e73f511.jpg"
+cocktail.save
+
+cocktail = Cocktail.new( name: "Amaretto fizz" )
+cocktail.remote_photo_url = "https://media.monin.com/us/media/catalog/product/B/u/Butter_Pecan_Amaretto_Sour-1534129787-0.png"
+cocktail.save
+
+cocktail = Cocktail.new( name: "Bitter orange & cardamom martinis" )
+cocktail.remote_photo_url = "https://www.bbcgoodfood.com/sites/default/files/styles/recipe/public/recipe_images/recipe-image-legacy-id--73620_10.jpg?itok=pqbEDV1V"
+cocktail.save
+
 
 puts 'Finished cocktails!'
