d79269f13f888b983b525bda896181d5263de4e9
Edoardo Olivieri <diddo.42@hotmail.it>
2019-04-04 16:17:19 +0100

    deploy

--- db/seeds.rb
+++ db/seeds.rb
@@ -26,12 +26,11 @@ cocktail = Cocktail.new( name: "Mojito" )
 cocktail.remote_photo_url = "https://cdn.liquor.com/wp-content/uploads/2018/09/04153106/mojito-720x720-recipe.jpg"
 cocktail.save
 
-cocktail = Cocktail.new( name: "
-Espresso martini" )
+cocktail = Cocktail.new(name: "Espresso martini")
 cocktail.remote_photo_url = "https://cdn.diffords.com/contrib/stock-images/2016/8/34/2016514176701b5d0ff8e94868a69e73f511.jpg"
 cocktail.save
 
-cocktail = Cocktail.new( name: "Amaretto fizz" )
+cocktail = Cocktail.new(name: "Amaretto fizz")
 cocktail.remote_photo_url = "https://media.monin.com/us/media/catalog/product/B/u/Butter_Pecan_Amaretto_Sour-1534129787-0.png"
 cocktail.save
 
